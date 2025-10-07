164253211607c2d68d4ac29f0ea990c0fd66c9a4
Felipe Rosa <fpreuli@gmail.com>
2025-10-07 13:04:34 -0300

    reorganizando controllers

--- src/main/java/br/senac/sp/appleacademyapi/student/StudentController.java
+++ src/main/java/br/senac/sp/appleacademyapi/student/StudentController.java
@@ -1,28 +1,51 @@
 package br.senac.sp.appleacademyapi.student;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/students") // Alterado para /students para consistência
+@RequestMapping("/students")
 public class StudentController {
 
     @Autowired
-    private StudentRepository studentRepository; // Corrigido para StudentRepository
+    private StudentRepository studentRepository;
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return studentRepository.save(student);
+    }
 
     @GetMapping
     public List<Student> getAllStudents() {
         return studentRepository.findAll();
     }
 
-    @PostMapping
-    public Student createStudent(@RequestBody Student student) {
-        return studentRepository.save(student);
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable Long id) {
+        return studentRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable Long id, @RequestBody Student studentDetails) {
+        return studentRepository.findById(id)
+                .map(student -> {
+                    student.setName(studentDetails.getName());
+                    student.setEmail(studentDetails.getEmail());
+                    Student updatedStudent = studentRepository.save(student);
+                    return ResponseEntity.ok(updatedStudent);
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteStudent(@PathVariable Long id) {
+        return studentRepository.findById(id)
+                .map(student -> {
+                    studentRepository.delete(student);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
     }
 }
--- src/main/java/br/senac/sp/appleacademyapi/student/TeamController.java
+++ src/main/java/br/senac/sp/appleacademyapi/student/TeamController.java
@@ -1,7 +1,8 @@
-package br.senac.sp.appleacademyapi.student; // Pacote correto
+package br.senac.sp.appleacademyapi.student;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,13 +12,20 @@ public class TeamController {
     @Autowired
     private TeamRepository teamRepository;
 
+    @PostMapping
+    public Team createTeam(@RequestBody Team team) {
+        return teamRepository.save(team);
+    }
+
     @GetMapping
     public List<Team> getAllTeams() {
         return teamRepository.findAll();
     }
 
-    @PostMapping
-    public Team createTeam(@RequestBody Team team) {
-        return teamRepository.save(team);
+    @GetMapping("/{id}")
+    public ResponseEntity<Team> getTeamById(@PathVariable Long id) {
+        return teamRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
