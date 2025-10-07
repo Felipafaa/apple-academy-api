1f76c31c625e2304adf7a0593aca33d71d2db60c
Felipe Rosa <fpreuli@gmail.com>
2025-10-07 04:57:27 -0300

    add spring-boot-starter-security dependency and simplify security configuration

--- src/main/java/br/senac/sp/appleacademyapi/security/SecurityConfiguration.java
+++ src/main/java/br/senac/sp/appleacademyapi/security/SecurityConfiguration.java
@@ -3,24 +3,15 @@ package br.senac.sp.appleacademyapi.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfiguration {
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable()) // Desativa proteção CSRF
-            .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll() // ✅ Permite tudo
-            )
-            .headers(headers -> headers.frameOptions(frame -> frame.disable())) // Permite H2-console se tiver
-            .formLogin(form -> form.disable())
-            .httpBasic(basic -> basic.disable());
-
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
 }
