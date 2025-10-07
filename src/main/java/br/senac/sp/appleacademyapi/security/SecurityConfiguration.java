89c3a758f4a34a01d6c3356dac5adc42c57657b0
Felipe Rosa <fpreuli@gmail.com>
2025-10-07 04:12:21 -0300

    config de security

---
+++ src/main/java/br/senac/sp/appleacademyapi/security/SecurityConfiguration.java
@@ -0,0 +1,47 @@
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Autowired
+    private AuthFilter authFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+            .authorizeHttpRequests( auth -> auth
+                .requestMatchers(HttpMethod.POST, "/auth/login").permitAll() // <-- AQUI ESTÁ A REGRA PRINCIPAL: LIBERA O LOGIN
+                .requestMatchers(HttpMethod.POST, "/users").permitAll()      // <-- BOA PRÁTICA: LIBERA O CADASTRO DE USUÁRIO TAMBÉM
+                .anyRequest().authenticated()                               // <-- TRANCA TODO O RESTO
+            )
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(sm -> sm.sessionCreationPolicy(STATELESS))
+            .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+            .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
