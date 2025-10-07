75188b3cf5d4fb84e2183518649e364e32e0c0df
Felipe Rosa <fpreuli@gmail.com>
2025-10-07 04:46:34 -0300

    mudança

--- src/main/java/br/senac/sp/appleacademyapi/security/SecurityConfiguration.java
+++ src/main/java/br/senac/sp/appleacademyapi/security/SecurityConfiguration.java
@@ -1,49 +1,26 @@
 package br.senac.sp.appleacademyapi.security;
 
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-    @Autowired
-    private AuthFilter authFilter;
-
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        return http
-            .authorizeHttpRequests( auth -> auth
-                .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                .requestMatchers(HttpMethod.POST, "/users").permitAll()
-                .anyRequest().authenticated()
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable()) // Desativa proteção CSRF
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll() // ✅ Permite tudo
             )
-            .csrf(csrf -> csrf.disable())
-            .sessionManagement(sm -> sm.sessionCreationPolicy(STATELESS))
-            .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
-            .build();
-    }
+            .headers(headers -> headers.frameOptions(frame -> frame.disable())) // Permite H2-console se tiver
+            .formLogin(form -> form.disable())
+            .httpBasic(basic -> basic.disable());
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
-        return config.getAuthenticationManager();
+        return http.build();
     }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-
 }
